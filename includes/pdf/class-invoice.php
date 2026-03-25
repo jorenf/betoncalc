772b88e343dea3e587ccc62d5ff4ce865e6b2f52
Jasper Scheper <jasper.scheper@gmail.com>
2026-03-25 21:30:02 +0100

    auto increment next year

--- includes/pdf/class-invoice.php
+++ includes/pdf/class-invoice.php
@@ -101,7 +101,7 @@ class Invoice extends PDF_Generator {
 		$prefix = get_option( 'boost_pdf_invoice_prefix', 'factuur-' );
 
 		// Get the last invoice number for this year.
-		$last_number = get_option( 'boost_invoice_last_number_' . $year, 0 );
+		$last_number = get_option( 'boost_invoice_last_number_' . $year, 99 );
 		$new_number  = $last_number + 1;
 
 		// Update the counter.
